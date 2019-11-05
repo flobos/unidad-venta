13ea8863c2065d453a8d6eb2dc08541ab758e0e1
flobos <fernandolobos@gmail.com>
2019-11-05 19:34:40 -0300

    test string 2

--- models/uom.py
+++ models/uom.py
@@ -5,12 +5,5 @@ class unidad_ventas_uom(models.Model):
     _inherit = 'product.uom'
     unidadventa_id = fields.Many2one('unidad_ventas.unidadventas', string="Unidad de Ventas", required=True)
 
-    @api.multi
-    def name_get(self):
-        result = []
-        for data_set in self:
-           nombre = 'hola'
-           result.append((data_set.id, nombre))
-        return result
-
+  
 
