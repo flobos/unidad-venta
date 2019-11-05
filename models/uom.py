4e4741d757e92457737a434cdfd578a35ba59857
flobos <fernandolobos@gmail.com>
2019-11-05 19:57:36 -0300

    fix parse

--- models/uom.py
+++ models/uom.py
@@ -5,5 +5,12 @@ class unidad_ventas_uom(models.Model):
     _inherit = 'product.uom'
     unidadventa_id = fields.Many2one('unidad_ventas.unidadventas', string="Unidad de Ventas", required=True)
 
-  
+    @api.multi
+    def name_get(self):
+        result = []
+        for obj in self:
+            name = name = '[' +str(obj.name) + ']' + ' ' + str(obj.unidadventa_id.name)
+            result.append((obj.id, name))
+        return result
+
 
