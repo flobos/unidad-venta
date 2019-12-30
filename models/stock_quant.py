d3cfd9cf24f6a184c233f4542c1346cbb346776e
Fernando Lobos <fernandolobos@gmail.com>
2019-12-30 16:23:07 -0300

    Round wth 2 dec

--- models/stock_quant.py
+++ models/stock_quant.py
@@ -10,6 +10,6 @@ class unidad_ventas_stock_quant(models.Model):
     @api.depends('reserved_quantity', 'quantity')
     def _calcular_unidades_disponible_venta(self):
         for rec in self:
-            factor = round(1 / rec.product_id.product_tmpl_id.uom_venta_id.factor)
-            rec.unidad_disponible_venta = round(rec.quantity - rec.reserved_quantity) / factor
+            factor = round(1 / rec.product_id.product_tmpl_id.uom_venta_id.factor, 2)
+            rec.unidad_disponible_venta = round(rec.quantity - rec.reserved_quantity, 2) / factor
 
